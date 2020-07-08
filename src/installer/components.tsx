a20dd312f4f6aeafae9c5f62f8f650002a4e7fb3
Matthew Congrove <mcongrove@pinpoint.com>
2020-07-08 16:21:06 -0500

    UI tweaks

--- src/installer/components.tsx
+++ src/installer/components.tsx
@@ -19,87 +19,103 @@ interface HeaderProps {
 export const Header = (props: HeaderProps) => {
 	return (
 		<div className={styles.Header}>
-			{
-				typeof(props.icon) === 'string' ? (
-					<img src={props.icon} alt={props.name} className={styles.Icon} />
-				) : (
-					<span className={styles.Icon}>
-						{props.icon}
-					</span>
-				)
-			}
+			<div className={styles.Row}>
+				{
+					typeof(props.icon) === 'string' ? (
+						<img src={props.icon} alt={props.name} className={styles.Icon} />
+					) : (
+						<span className={styles.Icon}>
+							{props.icon}
+						</span>
+					)
+				}
 
-			<div className={styles.Details}>
-				<div className={styles.Title}>
-					<div className={styles.Name}>
-						{props.name}
+				<div className={styles.Details}>
+					<div className={styles.Title}>
+						<div className={styles.Name}>
+							{props.name}
+						</div>
+
+						<div className={styles.Tags}>
+							{
+								props.tags.map(tag => (
+									<span key={tag} className={styles.Tag}>
+										{tag}
+									</span>
+								))
+							}
+						</div>
 					</div>
 
-					<div className={styles.Tags}>
-						{
-							props.tags.map(tag => (
-								<span key={tag} className={styles.Tag}>
-									{tag}
-								</span>
-							))
-						}
+					<div className={styles.Description}>
+						{props.description}
+					</div>
+
+					<div className={styles.Publisher}>
+						<span>
+							Published by
+						</span>
+				
+						<a target="_blank" href={props.publisher.url} rel="noopener noreferrer">
+							<img alt={props.publisher.name} src={props.publisher.avatar} />
+							{props.publisher.name}
+						</a>
 					</div>
 				</div>
 
-				<div className={styles.Description}>
-					{props.description}
-				</div>
-
-				<div className={styles.Publisher}>
-					<span>
-						Published by
-					</span>
-			
-					<a target="_blank" href={props.publisher.url} rel="noopener noreferrer">
-						<img alt={props.publisher.name} src={props.publisher.avatar} />
-						{props.publisher.name}
-					</a>
-				</div>
+				<Button
+					onClick={props.onClick}
+					color="Mono"
+					weight={300}
+					className={styles.Button}
+				>
+					<>
+						<Icon icon={['fas', 'key']} />
+						Change Login
+					</>
+				</Button>
 
 				{
-					props.hasError && (
+					props.installed ? (
+						<Button
+							onClick={props.onClick}
+							color="Red"
+							weight={500}
+							disabled={!props.enabled}
+							className={styles.Button}
+						>
+							<>
+								<Icon icon={['fas', 'trash']} />
+								Uninstall
+							</>
+						</Button>
+					) : (
+						<Button
+							onClick={props.onClick}
+							color="Green"
+							weight={500}
+							disabled={!props.enabled}
+							className={styles.Button}
+						>
+							<>
+								<Icon icon={['fas', 'check']} />
+								Install
+							</>
+						</Button>
+					)
+				}
+			</div>
+
+			{
+				props.hasError && (
+					<div className={styles.Row}>
 						<Banner error className={styles.Error}>
 							<>
 								<Icon color={Theme.Red500} icon={['fas', 'exclamation-triangle']} />
 								{props.errorMessage}
 							</>
 						</Banner>
-					)
-				}
-			</div>
-
-			{
-				props.installed ? (
-					<Button
-						onClick={props.onClick}
-						className={styles.InstallationButton}
-						color="Red"
-						weight={500}
-						disabled={!props.enabled}
-					>
-						<>
-							<Icon icon={['fas', 'trash']} />
-							Uninstall
-						</>
-					</Button>
-				) : (
-					<Button
-						onClick={props.onClick}
-						className={styles.InstallationButton}
-						color="Green"
-						weight={500}
-						disabled={!props.enabled}
-					>
-						<>
-							<Icon icon={['fas', 'check']} />
-							Install
-						</>
-					</Button>
+					</div>
 				)
 			}
 		</div>
