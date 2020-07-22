9c9ab2f0d41a03ad6ce192d073b38688960b4e7f
Jeff Haynie <jhaynie@pinpoint.com>
2020-07-22 10:30:07 -0500

    make forms be a bit more nice

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,5 @@
-import React, { FormEvent, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Icon, Tooltip } from '@pinpt/uic.next';
 import styles from './styles.less';
 import { useIntegration } from '../../useIntegration';
 import { IAuth, IAppBasicAuth, IAPIKeyAuth } from 'types';
@@ -9,41 +10,49 @@ export enum FormType {
 }
 
 interface FormProps {
-	type: FormType,
-	name: string,
-	callback: (auth: IAuth) => Promise<boolean>,
+	type: FormType;
+	name: string;
+	callback: (auth: IAuth) => Promise<boolean>;
 }
 
 export const Form = ({ type, name, callback }: FormProps) => {
-	const { config, setConfig, loading, setLoading } = useIntegration();
+	const { config, setConfig } = useIntegration();
 
+	const [disabled, setDisabled] = useState(true);
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 	const [apikey, setApiKey] = useState('');
 	const [url, setUrl] = useState('');
 
-	async function onSubmit() {
-		setLoading(true)
+	useEffect(() => {
 		if (type === FormType.BASIC) {
-			let auth = { username, password, url } as IAppBasicAuth
-			let success = await callback(auth);
-			if (!success) {
-				setLoading(false)
-				return;
-			}
-			config.basic_auth = auth;
+			setDisabled(!url || !username || !password);
 		} else {
-			let auth = { apikey, url } as IAPIKeyAuth;
-			let success = await callback(auth);
-			if (!success) {
-				setLoading(false)
-				return;
-			}
-			config.apikey_auth = auth;
+			setDisabled(!url || !apikey);
 		}
-		setConfig(config);
-		setLoading(false)
-	}
+	}, [username, password, apikey, url]);
+
+	const onClick = useCallback(() => {
+		const execute = async() => {
+			if (type === FormType.BASIC) {
+				let auth = { username, password, url } as IAppBasicAuth
+				let success = await callback(auth);
+				if (!success) {
+					return;
+				}
+				config.basic_auth = auth;
+			} else {
+				let auth = { apikey, url } as IAPIKeyAuth;
+				let success = await callback(auth);
+				if (!success) {
+					return;
+				}
+				config.apikey_auth = auth;
+			}
+			setConfig(config);
+		}
+		execute();
+	}, [config, username, password, apikey, url]);
 
 	return (
 		<div className={styles.Wrapper}>
@@ -56,10 +65,15 @@ export const Form = ({ type, name, callback }: FormProps) => {
 					Please provide the authentication credentials necessary to connect to your {name} instance.
 				</p>
 
-				<form onSubmit={(e: FormEvent<HTMLFormElement>) => e.preventDefault()}>
+				<div className={styles.Form}>
 					<div>
 						<label htmlFor="Form.URL">
 							Instance URL
+							<span>
+								<Tooltip content={<>The URL to use for connecting to your instance. The Agent must be able to reach this URL from the network location it will be installed.</>}>
+									<Icon icon="info-circle" />
+								</Tooltip>
+							</span>
 						</label>
 						<input type="text" name="Form.URL" className={styles.Wide} onChange={(e: any) => setUrl(e.target.value)} />
 					</div>
@@ -68,33 +82,45 @@ export const Form = ({ type, name, callback }: FormProps) => {
 						type === FormType.BASIC ? (
 							<>
 								<div>
-									<label htmlFor="Form.URL">
+									<label htmlFor="Form.USERNAME">
 										Username
+										<span>
+											<Tooltip content={<>The Username to use for connecting to your instance.</>}>
+												<Icon icon="info-circle" />
+											</Tooltip>
+										</span>
 									</label>
-									<input type="text" name="Form.USERNAME" onChange={(e: any) => setUsername(e.target.value)} />
+									<input type="text" name="Form.USERNAME" className={styles.Wide} onChange={(e: any) => setUsername(e.target.value)} />
 								</div>
 
 								<div>
-									<label htmlFor="Form.URL">
+									<label htmlFor="Form.PASSWORD">
 										Password
+										<span>
+											<Tooltip content={<>The Password to use for connecting to your instance.</>}>
+												<Icon icon="info-circle" />
+											</Tooltip>
+										</span>
 									</label>
-									<input type="text" name="Form.PASSWORD" onChange={(e: any) => setPassword(e.target.value)} />
+									<input type="password" name="Form.PASSWORD" className={styles.Wide} onChange={(e: any) => setPassword(e.target.value)} />
 								</div>
 							</>
 						) : (
 								<div>
-									<label htmlFor="Form.URL">
+									<label htmlFor="Form.APIKEY">
 										API Key
-								</label>
+										<span>
+											<Tooltip content={<>The APIKey which is used to connect to your instance.</>}>
+												<Icon icon="info-circle" />
+											</Tooltip>
+										</span>
+									</label>
 									<input type="text" name="Form.APIKEY" className={styles.Wide} onChange={(e: any) => setApiKey(e.target.value)} />
 								</div>
 							)
 					}
-
-					<div>
-						<input type="submit" value="Submit" onClick={onSubmit} />
-					</div>
-				</form>
+					<Button onClick={onClick} color="Green" weight={500} disabled={disabled} style={{ marginTop: '2.5rem' }}>Save Credentials</Button>
+				</div>
 			</div>
 		</div>
 	);
